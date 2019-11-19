1e6f7fec518b8d41cab49953ad6fecaeb2877758
DES-Destry <111andreyaa@gmail.com>
2019-11-19 21:46:49 +0600

    Added 3 style files: Fonts.xaml; Colors.xaml; Buttons.xaml; This is the style standart for my application.

---
+++ FileProtect/Model/BaseModelView.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FileProtect.Model
+{
+    class BaseModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ FileProtect/ModelView/MainModelView.cs
@@ -0,0 +1,18 @@
+﻿using FileProtect.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FileProtect.ModelView
+{
+    class MainModelView : BaseModelView
+    {
+        public MainModelView()
+        {
+
+        }
+    }
+}
